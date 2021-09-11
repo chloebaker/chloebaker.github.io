9e9814d0d823b66a8ff30541f13de9e4ab0059b1
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-09-10 22:19:07 -0500

    made function for "I'm just here for nekos" button

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $('#romance').click(function() {
+
+  var clearPage = function() {
+    document.getElementById("poster").innerHTML = "";
+    document.getElementById("animeTitle").innerHTML = "";
+    document.getElementById("animeDescription").innerHTML = "";
+    document.getElementById("nekoss").innerHTML = "";
+    document.getElementById("nekoButton").innerHTML = "";
+  };
+
+  var findAnime = function(category) {
     $.ajax({
       dataType: "json",
-      url: "https://kitsu.io/api/edge/anime?filter%5Bcategories%5D=romance",
+      url: "https://kitsu.io/api/edge/anime?filter%5Bcategories%5D=" + category,
       success: function(results) {
         console.log("good");
-        document.getElementById("poster").innerHTML = "";
-        document.getElementById("animeTitle").innerHTML = "";
-        document.getElementById("animeDescription").innerHTML = "";
+        clearPage();
         var numAnime = results["data"].length;
         var randomNum = Math.floor(Math.random() * (numAnime - 1));
         var anime =  results["data"][randomNum]["attributes"];
@@ -19,27 +26,31 @@ $(document).ready(function() {
         console.log(error);
       }
     });
+  };
+
+  $('#romance').click(function() {
+    findAnime("romance");
   });
 
   $('#magicalGirl').click(function() {
+    findAnime("magical%20girl");
+  });
+
+  $('#nekos').click(function() {
+    $("<button>").addClass("btn btn-primary").html("Give me a neko!").attr("id", "nekoImage").appendTo("#nekoButton");
+  })
+
+  $('#nekoImage').click(function() {
     $.ajax({
       dataType: "json",
-      url: "https://kitsu.io/api/edge/anime?filter%5Bcategories%5D=magical%20girl",
+      url: "https://nekos.best/api/v1/nekos",
       success: function(results) {
         console.log("good");
-        document.getElementById("poster").innerHTML = "";
-        document.getElementById("animeTitle").innerHTML = "";
-        document.getElementById("animeDescription").innerHTML = "";
-        var numAnime = results["data"].length;
-        var randomNum = Math.floor(Math.random() * (numAnime - 1));
-        var anime =  results["data"][randomNum]["attributes"];
-        $("<img>").attr("src", anime["posterImage"]["medium"]).appendTo("#poster");
-        $("<h2>").text(anime["canonicalTitle"]).appendTo("#animeTitle");
-        $("<p>").text(anime["synopsis"]).appendTo("#animeDescription");
-      },
+        $("<img>").attr("src", results["url"]).appendTo("#nekoss");
       error: function(xhr,status,error) {
         console.log(error);
       }
     });
-  });
+  })
+
 });
