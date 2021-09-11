0e57a030dc56fe09bddb2c057bd92a1dccce81c4
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-09-10 23:37:56 -0500

    edited findAnime function, added comments

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   var clearPage = function() {
-    document.getElementById("poster").innerHTML = "";
+    document.getElementById("poster").innerHTML = "";         // https://stackoverflow.com/questions/5744233/how-to-empty-the-content-of-a-div/5744262
     document.getElementById("animeTitle").innerHTML = "";
     document.getElementById("animeDescription").innerHTML = "";
   };
@@ -15,11 +15,15 @@ $(document).ready(function() {
         clearPage();
         document.getElementById("nekoImages").innerHTML = "";
         var numAnime = results["data"].length;
-        var randomNum = Math.floor(Math.random() * (numAnime - 1));
+        var randomNum = Math.floor(Math.random() * (numAnime - 1));         // https://www.codegrepper.com/code-examples/javascript/jquery+random+number+between learned how to generate a random number, used it to generate a random index of results["data"] (the list of animes in that category)
         var anime =  results["data"][randomNum]["attributes"];
-        $("<img>").attr("src", anime["posterImage"]["medium"]).appendTo("#poster");
-        $("<h2>").text(anime["canonicalTitle"]).appendTo("#animeTitle");
-        $("<p>").text(anime["synopsis"]).appendTo("#animeDescription");
+        if (anime["nsfw"]) {
+          findAnime(category);
+        } else {
+          $("<img>").attr("src", anime["posterImage"]["medium"]).appendTo("#poster");
+          $("<h2>").text(anime["canonicalTitle"]).appendTo("#animeTitle");
+          $("<p>").text(anime["synopsis"]).appendTo("#animeDescription");
+        }
       },
       error: function(xhr,status,error) {
         console.log(error);
