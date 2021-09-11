5306455efb3ae1942749f6bf80c6aef085860568
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-09-10 20:31:09 -0500

    added log to console

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
       dataType: "json",
       url: "https://kitsu.io/api/edge/anime?filter%5Bcategories%5D=romance",
       success: function(results) {
+        console.log("good");
         document.getElementById("poster").innerHTML = "";
         document.getElementById("animeTitle").innerHTML = "";
         document.getElementById("animeDescription").innerHTML = "";
@@ -24,6 +25,7 @@ $(document).ready(function() {
       dataType: "json",
       url: "https://kitsu.io/api/edge/anime?filter%5Bcategories%5D=magical%20girl",
       success: function(results) {
+        console.log("good");
         document.getElementById("poster").innerHTML = "";
         document.getElementById("animeTitle").innerHTML = "";
         document.getElementById("animeDescription").innerHTML = "";
